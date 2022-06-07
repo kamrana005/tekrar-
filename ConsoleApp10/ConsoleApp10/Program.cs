7f903814107efbe472a9be2921d7c7bd937be024
kamrana005 <kamranda@gmail.edu.az>
2022-06-07 13:01:29 +0400

    file

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student
+            {
+                FullName = "Kamran",
+                GroupNo = "P230",
+                
+            };
+            Student student2 = new Student
+            {
+                FullName = "Pasa",
+                GroupNo = "P220",
+
+            }; 
+            Student student3 = new Student
+            {
+                FullName = "Yasin",
+                GroupNo = "P210",
+
+            };
+            Console.WriteLine(student1);
+            Console.WriteLine("Total Students:" +Student.TotalCount);
+        }
+    }
+}
---
+++ ConsoleApp10/ConsoleApp10/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    internal class Student
+    {
+        static Student()
+        {
+            _totalCount = 0;
+        }
+        public Student()
+        {
+            _totalCount++;
+            No = _totalCount;
+        }
+
+        private static int _totalCount;
+        public int TotalCount => _totalCount;
+        public int No { get;}
+        public string FullName;
+        private string _groupNo;
+        public string GroupNo
+        {
+            get { return _groupNo; }
+            set { if(CheckGroupNo(value)) _groupNo = value; }
+            
+        }
+        public bool CheckGroupNo(string GroupNo)
+        {
+            if (!string.IsNullOrEmpty(GroupNo) && GroupNo == 4)
+            {
+                if (Char.IsLetter(GroupNo[0]))
+                {
+                    for(int i = 1; i < GroupNo.Length; i++)
+                    {
+                        if(!Char.IsDigit(GroupNo[i])) 
+                            return false;
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
